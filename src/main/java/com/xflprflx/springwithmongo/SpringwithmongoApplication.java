d19c4472cafe1b6364b768d17dc5241448108b05
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-10-18 13:27:24 -0300

    primeiro commit - projeto criado

---
+++ src/main/java/com/xflprflx/springwithmongo/SpringwithmongoApplication.java
@@ -0,0 +1,13 @@
+package com.xflprflx.springwithmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xflprflx/springwithmongo/SpringwithmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xflprflx.springwithmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwithmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
