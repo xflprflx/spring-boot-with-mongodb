f744b8cb1f74ccb3454af3dd535c54f0d6d15e29
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-10-20 12:02:20 -0300

    Consulta simples com @Query

--- src/main/java/com/xflprflx/springwithmongo/services/PostService.java
+++ src/main/java/com/xflprflx/springwithmongo/services/PostService.java
@@ -24,7 +24,7 @@ public class PostService {
     }
 
     public List<PostDTO> findByTitle(String text){
-        List<Post> postList = postRepository.findByTitleContainingIgnoreCase(text);
+        List<Post> postList = postRepository.searchTitle(text);
         List<PostDTO> posts = postList.stream().map(post -> new PostDTO(post)).collect(Collectors.toList());
         return posts;
     }
